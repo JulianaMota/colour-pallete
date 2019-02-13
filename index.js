7a95567f4708bb824f76c39495d7975e8d967ea6
Juliana Mota <julianamota89@gmail.com>
2019-02-13 14:43:53 +0100

    rgb code to hsl coder

--- index.js
+++ index.js
@@ -16,28 +16,68 @@ function colorValue() {
 }
 
 function updateFirst(event) {
-  let hexCode = document.querySelector(".hexCode");
   let color1 = document.querySelector(".color1");
+  let hexCode = document.querySelector(".hexCode span");
   if (color1) {
     color1.style.backgroundColor = event.target.value;
     hexCode.textContent = event.target.value;
+    console.log(event.target.value);
+    let rgbCode = color1.style.backgroundColor;
+    console.log(rgbCode);
+    rgbCodeFuntion(color1.style.backgroundColor);
   }
 }
 
-/*colorPicker.addEventListener("input", updateFirst, false);
-colorPicker.addEventListener("chan\ge", watchColorPicker, false);
+function rgbCodeFuntion(rgbCode) {
+  console.log(rgbCode);
+  const sliceRGB = rgbCode.slice(4, -1);
+  const splitRGB = sliceRGB.split(", ");
+  const r = splitRGB[0];
+  const g = splitRGB[1];
+  const b = splitRGB[2];
+  console.log(r, g, b);
 
-function watchColorPicker(event) {
-  document.querySelectorAll("p").forEach(function(p) {
-    p.style.color = event.target.value;
-  });
-}*/
-
-/*function getColor(e) {
-  e.preventDefault();
-  const colorValue = document.getElementById("color1").value;
-  console.log(colorValue);
-  document.querySelector(".color1").innerHTML = colorValue;
+  RGBtoHSL(r, g, b);
 }
 
-getColor();*/
+function RGBtoHSL(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let h, s, l;
+
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  if (max === min) {
+    h = 0;
+  } else if (max === r) {
+    h = 60 * (0 + (g - b) / (max - min));
+  } else if (max === g) {
+    h = 60 * (2 + (b - r) / (max - min));
+  } else if (max === b) {
+    h = 60 * (4 + (r - g) / (max - min));
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+  // multiply s and l by 100 to get the value in percent, rather than [0,1]
+  s *= 100;
+  l *= 100;
+
+  console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
+}
+
+/*function getHexNumber() {
+  console.log(hexCode);
+}*/
